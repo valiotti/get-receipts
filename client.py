c315d97e203f839d9313922e6ef002e148dcab28
aeksei <aeksei94@gmail.com>
2020-08-18 17:59:47 +0300

    add auth

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
+import os
 import json
+
 import requests
+from dotenv import load_dotenv
 
 
 class NalogRuPython:
@@ -10,10 +13,41 @@ class NalogRuPython:
     ACCEPT = '*/*'
     USER_AGENT = 'billchecker/2.9.0 (iPhone; iOS 13.6; Scale/2.00)'
     ACCEPT_LANGUAGE = 'ru-RU;q=1, en-US;q=0.9'
-    IF_NONE_MATCH = 'W/"39-ecMKyjU/fhuUBv4JoLNnlATwSM0"'
 
     def __init__(self):
-        self.__session_id = '5b1bdc9538ded657352567f1:36d497a9-78ab-4d55-a586-2961aaa169dd'
+        load_dotenv()  # read environments from .env file
+        self.__session_id = None
+        self.set_session_id()
+
+    def set_session_id(self) -> None:
+        """
+        Authorization using INN and password of user lk nalog.ru
+        """
+        if os.getenv('CLIENT_SECRET') is None:
+            raise ValueError('OS environments not content "CLIENT_SECRET"')
+        if os.getenv('INN') is None:
+            raise ValueError('OS environments not content "INN"')
+        if os.getenv('PASSWORD') is None:
+            raise ValueError('OS environments not content "PASSWORD"')
+
+        url = f'https://{self.HOST}/v2/mobile/users/lkfl/auth'
+        payload = {
+            'inn': os.getenv('INN'),
+            'client_secret': os.getenv('CLIENT_SECRET'),
+            'password': os.getenv('PASSWORD')
+        }
+        headers = {
+            'Host': self.HOST,
+            'Accept': self.ACCEPT,
+            'Device-OS': self.DEVICE_OS,
+            'Device-Id': self.DEVICE_ID,
+            'clientVersion': self.CLIENT_VERSION,
+            'Accept-Language': self.ACCEPT_LANGUAGE,
+            'User-Agent': self.USER_AGENT,
+        }
+
+        resp = requests.post(url, json=payload, headers=headers)
+        self.__session_id = resp.json()['sessionId']
 
     def get_ticket_id(self, qr: str) -> str:
         """
@@ -29,14 +63,13 @@ class NalogRuPython:
             'Accept': self.ACCEPT,
             'Device-OS': self.DEVICE_OS,
             'Device-Id': self.DEVICE_ID,
-            'If-None-Match': self.IF_NONE_MATCH,
             'clientVersion': self.CLIENT_VERSION,
             'Accept-Language': self.ACCEPT_LANGUAGE,
             'sessionId': self.__session_id,
             'User-Agent': self.USER_AGENT,
         }
 
-        resp = requests.post(url, json=payload, headers=headers, verify=False)
+        resp = requests.post(url, json=payload, headers=headers)
 
         return resp.json()["id"]
 
@@ -59,7 +92,7 @@ class NalogRuPython:
             'Accept-Language': self.ACCEPT_LANGUAGE,
         }
 
-        resp = requests.get(url, headers=headers, verify=False)
+        resp = requests.get(url, headers=headers)
 
         return resp.json()
 
