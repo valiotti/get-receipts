ec209db320e47029404d456f072cdfec8cd014e7
aeksei <aeksei94@gmail.com>
2020-08-19 12:08:58 +0300

    change qr_code and fix json decode

--- nalog_python.py
+++ nalog_python.py
@@ -100,6 +100,6 @@ class NalogRuPython:
 
 if __name__ == '__main__':
     client = NalogRuPython()
-    qr_code = "t=20200727T1117&s=4850.00&fn=9287440300634471&i=13571&fp=3730902192&n=1"
+    qr_code = "t=20200709T2008&s=7273.00&fn=9282440300688488&i=14186&fp=1460060363&n=1"
     ticket = client.get_ticket(qr_code)
-    print(json.dumps(ticket, indent=4))
+    print(json.dumps(ticket, indent=4, ensure_ascii=False))
